0361eb768dc69a6323d8b5a7c69551f31a863ac5
Unknown <reynaldo.garcia@ttu.edu>
2017-02-19 18:13:15 -0600

    Added Scripts for the UI's functionality
    Added a folder for the UI's scrpits and three scripts for the main menu's functionality.

---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,14 @@
+﻿// Credit goes to: https://unity3d.com/learn/tutorials/topics/user-interface-ui/creating-main-menu
+
+using UnityEngine;
+using System.Collections;
+
+public class QuitOnClick : MonoBehaviour {
+	public void Quit(){
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,26 @@
+﻿// Credit goes to: https://unity3d.com/learn/tutorials/topics/user-interface-ui/creating-main-menu
+
+using UnityEngine;
+using System.Collections;
+using Unity.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+	public EventSystem evenSystem;
+	public GameObject selectedObject;
+	private bool buttonSelected;
+
+	// Use this for initialization.
+	void Start() {}
+
+	// Update is called once per frame.
+	void Update() {
+		if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == fase) {
+			eventSystem.SetSelectedGameObject(selectedObject);
+			buttonSelected = true;
+		}
+	}
+
+	private void OnDisable(){
+		buttonSelected = false;
+	}
+}
