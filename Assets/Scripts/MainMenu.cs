9b9230dfd9f4d358a60727df095f43ce61062366
Unknown <reynaldo.garcia@ttu.edu>
2017-02-25 10:49:06 -0600

    Added some comments
    Added some important comments. Basically, just to let you all know why a certain thing happens.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,9 @@ public class MainMenu : MonoBehaviour {
 		if (isStart) {
 			Application.LoadLevel(0);
 		}if (isQuit) {
+			// Application.Quit() only works when the game is launched as a desktop application...
 			Application.Quit();
+			// so the exit button's will change to green as a way of testing it in Unity.
 			GetComponent<Renderer>().material.color = Color.green;
 		}
 	}
