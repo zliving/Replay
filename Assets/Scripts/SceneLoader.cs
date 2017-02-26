2fb61d93e16837d81fb527f478f5c451539b7c52
Unknown <reynaldo.garcia@ttu.edu>
2017-02-25 18:25:50 -0600

    Fixed a bug
    Fixed a bug that prevented the loading scene from going to the main menu. They now both transition to each other, proving their correct functionality.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour {
+	// Use a boolean to specify that a new scene is not loading.
 	private bool loadScene = false;
 	[SerializeField]
 	private int scene;
@@ -10,20 +11,20 @@ public class SceneLoader : MonoBehaviour {
 	private Text loadingText;
 
 	void Update() {
-		// If the player has pressed the space bar and a new scene is not loading yet...
-		if (Input.GetKeyUp(KeyCode.Space) && loadScene == false) {
+		// If a new scene is not loading...
+		if (loadScene == false) {
 			// ...set the loadScene boolean to true to prevent loading a new scene more than once...
 			loadScene = true;
 			// ...change the instruction text to read "Loading..."
 			// TODO: chang flashing text to Replay
-			loadingText.text = "Loading...";
+			loadingText.text = "Loading";
 			// ...and start a coroutine that will load the desired scene.
 			StartCoroutine(LoadNewScene());
 		} // If the new scene has started loading...
 		if (loadScene == true) {
 			// ...then pulse the transparency of the loading text
-			// TODO: only pulse the brightness of the symbol or pulse the brightness of all of the logo.
-			loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b,
+			// TODO: customize the animation to either the replay text pulsing or the circular arrow rotating.
+			loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, 
 				Mathf.PingPong(Time.time, 1));
 		}
 	}
@@ -31,10 +32,9 @@ public class SceneLoader : MonoBehaviour {
 	// The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
 	IEnumerator LoadNewScene() {
 		// This line waits for 3 seconds before executing the next line in the coroutine.
-		// This line is only necessary for this demo. The scenes are so simple that they load too fast to read the "Loading..." text.
 		yield return new WaitForSeconds(3);
 		// Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
-		AsyncOperation async = Application.LoadLevelAsync(scene);
+		AsyncOperation async = Application.LoadLevelAsync(1);		//the integer represents the scene being loaded.
 		// While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
 		while (!async.isDone) {
 			yield return null;
