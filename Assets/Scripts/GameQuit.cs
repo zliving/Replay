c5f6c3962e6afe96405bfc65db5f73979a15e4f4
Unknown <reynaldo.garcia@ttu.edu>
2017-03-05 11:13:02 -0600

    Refactored a bit

--- Assets/Scripts/GameQuit.cs
+++ Assets/Scripts/GameQuit.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameQuit : MonoBehaviour {
-	public void QuitOnClick(bool isClicked) {
-		if (isClicked) {
-			// Application.Quit() only works when the game is launched as a desktop application...
-			Application.Quit ();
-			// so the exit button's will change to green as a way of testing it in Unity.
-			GetComponent<Renderer> ().material.color = Color.green;
+
+	public void Update() {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit();
 		}
 	}
+
+	public void QuitOnClick(bool isClicked) {
+		// Application.Quit() only works when the game is launched as a desktop application...
+		Application.Quit();
+		// Unlike in the previous GUI I can't change the color on a button.
+	}
+		
 }
