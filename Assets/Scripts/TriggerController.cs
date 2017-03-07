6a58e4e70adafc191a5ace7574d9a68ac9ba6f53
Chris.V <cvasqu09@gmail.com>
2017-03-06 23:18:00 -0600

    Added the trigger controller script.

---
+++ Assets/Scripts/TriggerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class TriggerController {
+	private bool playerOnTrigger = false;
+
+	private ITrigger trigger;
+
+	public void OnTriggerEnter(Collider c){
+		trigger.Enter (c, ref playerOnTrigger);
+	}
+
+	public void OnTriggerExit(Collider c){
+		trigger.Exit(c, ref playerOnTrigger);
+	}
+
+	public bool isPlayerOnTrigger(){
+		return playerOnTrigger;
+	}
+
+	public void setTriggerInterface(ITrigger t){
+		trigger = t;
+	}
+}
