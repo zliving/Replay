789ba68a3941e8ec3edfbcfd81b3a2416ba67a13
Andrew <stra.tbone@gmail.com>
2017-03-13 23:47:44 -0500

    Added TestInventory script
    TestInventory holds the picked up items from ItemPickup.cs in a list called inventory.

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour {
 
+    public GameObject itemParent;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,6 +18,8 @@ public class ItemPickup : MonoBehaviour {
 
 	void OnMouseDown () {
         this.GetComponent<MeshRenderer>().enabled = false;		//Disables renderer on click
-		this.GetComponent<Collider>().enabled = false;	//Disables collider on click
-	}
+		this.GetComponent<Collider>().enabled = false;  //Disables collider on click
+        GameObject.Find("GlobalScripts").GetComponent<TestInventory>().addItem(itemParent);    //Send object data to the inventory
+
+    }
 }
---
+++ Assets/Scripts/TestInventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInventory : MonoBehaviour {
+
+    List<GameObject> inventory = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+          
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //addItem is called from itemPickup and passes the object being picked up
+    public void addItem(GameObject pickup) {
+        inventory.Add(pickup);  //Add the picked up item to the inventory
+
+        //for debug purposes show names of GameObjects in Inventory
+        foreach (var item in inventory) {
+            Debug.Log(item.name);
+        }
+    }
+
+    //removeItem is called from itemPutdown and passes the object being put down
+    /*public void removeItem(GameObject putdown) {
+        for(int i = 0; i < inventory.Count - 1; i++) {
+            if (inventory[i] == putdown) {
+                inventory.Remove(i);
+            }
+        }    
+    }*/
+}
