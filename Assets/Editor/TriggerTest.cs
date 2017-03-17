028715783e82a7132a93357fca745bb382434239
Chris.V <cvasqu09@gmail.com>
2017-03-16 19:53:41 -0500

    Added the TriggerTest to editor folder

---
+++ Assets/Editor/TriggerTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using 
+public class TriggerTest {
+
+	[Test]
+	public void TriggerContoller_OnTriggerEnter_Triggered() {
+		//Arrange
+		TriggerController tController = new TriggerController();
+
+
+		//Assert
+		//The object has a new name
+		Assert.AreEqual(newGameObjectName, gameObject.name);
+	}
+}
