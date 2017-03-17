960d94bf1f32ea4cf2bb459ce8e77f817c2deba7
Chris.V <cvasqu09@gmail.com>
2017-03-16 20:59:19 -0500

    Finished adding the test for the isPlayerOnTrigger_True

--- Assets/Editor/TriggerTest.cs
+++ Assets/Editor/TriggerTest.cs
@@ -1,19 +1,45 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using NUnit.Framework;
-using 
+using RAIN.Core;
+
 public class TriggerTest {
 
 	[Test]
-	public void TriggerContoller_OnTriggerEnter_Triggered() {
-		//Arrange
-		TriggerController tController = new TriggerController();
+	public void TriggerContoller_isPlayerOnTrigger_True() {
+		// Create trigger object
+		GameObject trigger = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		trigger.AddComponent<CounterTrigger> ();
+		CounterTrigger counterTrigger = trigger.GetComponent<CounterTrigger> ();
+
+		// Add ai with rig and tag for the "Enter" function inside of CounterTrigger.cs
+		GameObject ai = GameObject.CreatePrimitive (PrimitiveType.Capsule);
+		ai.AddComponent<AIRig> ();
+		ai.tag = "AITag";
+
+		// Create a player with collider
 		GameObject go = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		BoxCollider bCollider = new BoxCollider ();
-		go.AddComponent<Collider> (bCollider);
+		go.AddComponent<BoxCollider> ();
+		Collider c = go.GetComponent<BoxCollider> ();
+
+		// Change tag for trigger to recognize
 		go.tag = "PlayerTag";
-		tController.OnTriggerEnter (go.GetComponent<Collider> ());
-		Assert.IsTrue(
+	
+		// Make sure that controller is not null
+		counterTrigger.controller = new TriggerController();
+
+		// Set the interface so that it is not null when trying to make method calls.
+		counterTrigger.controller.setTriggerInterface (counterTrigger);
+
+		// Call the event trigger
+		counterTrigger.OnTriggerEnter (c);
+
+		// Assert
+		Assert.IsTrue (counterTrigger.controller.isPlayerOnTrigger ());
+	}
+
+	[Test]
+	public void TriggerController_isPlayerOnTrigger_False() {
 
 	}
 }
