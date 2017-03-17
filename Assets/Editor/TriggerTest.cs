3f2ef9c0f61381412619f09883e80a4a91519d5c
Chris.V <cvasqu09@gmail.com>
2017-03-16 21:08:30 -0500

    Added another test case for when the player exits a trigger

--- Assets/Editor/TriggerTest.cs
+++ Assets/Editor/TriggerTest.cs
@@ -6,7 +6,7 @@ using RAIN.Core;
 public class TriggerTest {
 
 	[Test]
-	public void TriggerContoller_isPlayerOnTrigger_True() {
+	public void TriggerContoller_isPlayerOnTriggerEntered_True() {
 		// Create trigger object
 		GameObject trigger = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		trigger.AddComponent<CounterTrigger> ();
@@ -39,7 +39,7 @@ public class TriggerTest {
 	}
 
 	[Test]
-	public void TriggerController_isPlayerOnTrigger_False() {
+	public void TriggerController_isPlayerOnTriggerEntered_False() {
 		// Create trigger object
 		GameObject trigger = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		trigger.AddComponent<CounterTrigger> ();
@@ -70,4 +70,38 @@ public class TriggerTest {
 		// Assert
 		Assert.IsFalse (counterTrigger.controller.isPlayerOnTrigger ());
 	}
+
+	[Test]
+	public void TriggerController_isPlayerOnTriggerExit_False() {
+		// Create trigger object
+		GameObject trigger = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		trigger.AddComponent<CounterTrigger> ();
+		CounterTrigger counterTrigger = trigger.GetComponent<CounterTrigger> ();
+
+		// Add ai with rig and tag for the "Enter" function inside of CounterTrigger.cs
+		GameObject ai = GameObject.CreatePrimitive (PrimitiveType.Capsule);
+		ai.AddComponent<AIRig> ();
+		ai.tag = "AITag";
+
+		// Create a object that does not have "PlayerTag" with collider
+		GameObject go = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		go.AddComponent<BoxCollider> ();
+		Collider c = go.GetComponent<BoxCollider> ();
+
+		// Change tag for trigger to recognize
+		go.tag = "PlayerTag";
+
+		// Make sure that controller is not null
+		counterTrigger.controller = new TriggerController();
+
+		// Set the interface so that it is not null when trying to make method calls.
+		counterTrigger.controller.setTriggerInterface (counterTrigger);
+
+		// Call the event trigger
+		counterTrigger.OnTriggerExit (c);
+
+		// Assert
+		Assert.IsFalse (counterTrigger.controller.isPlayerOnTrigger ());
+	}
+		
 }
