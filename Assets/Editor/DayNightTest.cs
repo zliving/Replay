24e9bd20a35718ee15b017881ffcb012b70fbb5d
Chris.V <cvasqu09@gmail.com>
2017-03-25 17:20:18 -0500

    Added a method to TOD manager, and test for night and day when true

---
+++ Assets/Editor/DayNightTest.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using AC.TimeOfDaySystemFree;
+
+public class DayNightTest {
+
+	[Test]
+	public void isDay_True() {
+		// Time of Day object
+		var TODObject = new GameObject();
+		TODObject.AddComponent<TimeOfDayManager> ();
+
+		// Get time manager
+		TimeOfDayManager time = TODObject.GetComponent<TimeOfDayManager> ();
+
+		// Assign day settings
+		time.dayInSeconds = 60.0f;
+
+		// Set the time to 1 p.m.
+		time.timeline = 13;
+
+		// Update time to determine night or day.
+		time.updateTime ();
+	
+		Assert.IsTrue (time.IsDay, "Not daytime");
+	}
+
+	[Test]
+	public void isNight_True() {
+		// Time of Day object
+		var TODObject = new GameObject();
+		TODObject.AddComponent<TimeOfDayManager> ();
+
+		// Get time manager
+		TimeOfDayManager time = TODObject.GetComponent<TimeOfDayManager> ();
+
+		// Assign day settings
+		time.dayInSeconds = 60.0f;
+
+		// Set the time to 1 a.m.
+		time.timeline = 1;
+
+		// Update time to determine night or day.
+		time.updateTime ();
+
+		Assert.IsTrue (time.IsNight, "Not nighttime");
+	}
+}
