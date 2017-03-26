2ca7a9e8f730fbc49393b70ac33b3f7406ebf0c5
Chris.V <cvasqu09@gmail.com>
2017-03-25 20:49:45 -0500

    Added tests for false day and night cases

--- Assets/Editor/DayNightTest.cs
+++ Assets/Editor/DayNightTest.cs
@@ -46,4 +46,67 @@ public class DayNightTest {
 
 		Assert.IsTrue (time.IsNight, "Not nighttime");
 	}
+
+	[Test]
+	public void isDay_False() {
+		// Time of Day object
+		var TODObject = new GameObject();
+		TODObject.AddComponent<TimeOfDayManager> ();
+
+		// Get time manager
+		TimeOfDayManager time = TODObject.GetComponent<TimeOfDayManager> ();
+
+		// Assign day settings
+		time.dayInSeconds = 60.0f;
+
+		// Set the time to 1 a.m.
+		time.timeline = 1;
+
+		// Update time to determine night or day.
+		time.updateTime ();
+
+		Assert.IsFalse (time.IsDay, "Is daytime");
+	}
+
+	[Test]
+	public void isNight_False() {
+		// Time of Day object
+		var TODObject = new GameObject();
+		TODObject.AddComponent<TimeOfDayManager> ();
+
+		// Get time manager
+		TimeOfDayManager time = TODObject.GetComponent<TimeOfDayManager> ();
+
+		// Assign day settings
+		time.dayInSeconds = 60.0f;
+
+		// Set the time to 3 p.m.
+		time.timeline = 15;
+
+		// Update time to determine night or day.
+		time.updateTime ();
+
+		Assert.IsFalse (time.IsNight, "Is nighttime");
+	}
+		
+	[Test]
+	public void sunRotation_Noon_90() {
+		// Time of Day object
+		var TODObject = new GameObject();
+		TODObject.AddComponent<TimeOfDayManager> ();
+
+		// Get time manager
+		TimeOfDay time = TODObject.GetComponent<TimeOfDayManager> ();
+
+		// Assign day settings
+		time.dayInSeconds = 60.0f;
+
+		// Set the time to 1 a.m.
+		time.timeline = 1;
+
+		// Update time to determine night or day.
+		time.updateTime ();
+
+		Assert.IsFalse (time.IsDay, "Is daytime");
+	}
 }
