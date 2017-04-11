5ce7490c0c3c70f4eb232df093db39cd2c387efc
Andrew <stra.tbone@gmail.com>
2017-04-11 00:04:22 -0500

    Changed Pickup Control to "E" key

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -7,10 +7,15 @@ public class ItemPickup : MonoBehaviour {
     public GameObject itemParent;
 
     // OnMouseDown() is called when the mouse clicks on the GameObject collider
-	public void OnMouseDown () {
-        // Disable the renderer and collider on the GameObject default child
-        this.GetComponent<MeshRenderer>().enabled = false;
-		this.GetComponent<Collider>().enabled = false;
+	public void OnMouseOver () {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Disable the renderer and collider on the GameObject default child
+            this.GetComponent<MeshRenderer>().enabled = false;
+            this.GetComponent<Collider>().enabled = false;
+        }
+            
 
         // Send the pickupitem object data to the inventory
         GameObject.Find("GlobalScripts").GetComponent<TestInventory>().addItem(itemParent);
