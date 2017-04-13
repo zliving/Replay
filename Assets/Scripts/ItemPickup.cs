3a000cbb05fd2c6f6bd1a7946d6915179ff56129
Andrew <stra.tbone@gmail.com>
2017-04-13 16:53:20 -0500

    Cleaned code for itemPickup.cs

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour {
 
-    public GameObject draggingObject;
-    public GameObject playerCamera;
-    float distance = 1.5f;
-    bool isDragging = false;
-
     float speed = 5;
     float maxSpeed = 15;
     private Transform dragObj = null;
@@ -17,59 +12,38 @@ public class ItemPickup : MonoBehaviour {
 
     // Update is called once per frame 
     void Update() {
-
-        if (Input.GetMouseButton(0))
-        {  // if left mouse button pressed...
-           // cast a ray from the mouse pointer
+        // Check if the mouse button is being pressed
+        if (Input.GetMouseButton(0)) {  
+            // Cast a ray from the FPS camera to the item
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // No item is currently being dragged
             if (!dragObj)
-            {  // if nothing picked yet...
-               // and the ray hit some rigidbody...
+            { 
+                // Check for a RaycastHit on an item
                 if (Physics.Raycast(ray, out hit) && hit.rigidbody)
                 {
-                    dragObj = hit.transform;  // save picked object's transform
-                    length = hit.distance;  // get "distance from the mouse pointer"
+                    // Store the previous location of the item
+                    dragObj = hit.transform;
+                    // Store the distance between the item and the mouse
+                    length = hit.distance;  
                 }
             }
             else
-            {  // if some object was picked...
-               // calc velocity necessary to follow the mouse pointer
+            {
+                // Determine the velocity needed to move the item smoothly
                 var vel = (ray.GetPoint(length) - dragObj.position) * speed;
-                // limit max velocity to avoid pass through objects
+                // Limit the velocity to prevent collisions
                 if (vel.magnitude > maxSpeed) vel *= maxSpeed / vel.magnitude;
-                // set object velocity
+                // Set the velocity of the item being dragged
                 dragObj.GetComponent<Rigidbody>().velocity = vel;
             }
         }
         else
-        {  // no mouse button pressed
-            dragObj = null;  // dragObj free to drag another object
+        {
+            // Reset dragObj so a new item can be selected
+            dragObj = null;  
         }
-    }
-
-    private GameObject GetObjectFromMouseRaycast() {
-
-        GameObject gmObj = null;
-        RaycastHit hitInfo = new RaycastHit();
-        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
-
-        if (hit) {
-
-            if (hitInfo.collider.gameObject.GetComponent<Rigidbody>() && Vector3.Distance(hitInfo.collider.gameObject.transform.position,transform.position) <= distance) {
-                gmObj = hitInfo.collider.gameObject;
-            }
-        }
-
-        return gmObj;
-    }
-
-    private Vector3 CalculateMouse3DVector() {
-
-        Vector3 mouseDrag = Input.mousePosition;
-        mouseDrag.z = distance;
-        mouseDrag = Camera.main.ScreenToWorldPoint(mouseDrag);
-
-        return mouseDrag;
     }
 
     // OnMouseOver() is called when the mouse hovers over the GameObject collider
@@ -82,46 +56,6 @@ public class ItemPickup : MonoBehaviour {
         }
             
         // Send the pickupitem object data to the inventory
-        GameObject.Find("GlobalScripts").GetComponent<TestInventory>().addItem(draggingObject);
+        //GameObject.Find("GlobalScripts").GetComponent<TestInventory>().addItem(/*dragObj*/);
     }
 }
-
-//if (Input.GetMouseButton(0)) {
-
-//    if (!isDragging) {
-
-//        draggingObject = GetObjectFromMouseRaycast();
-
-//        if (draggingObject) {
-
-//            //draggingObject.GetComponent<Rigidbody>().isKinematic = true;
-//            isDragging = true;
-
-//        }
-//    }
-//    else if (draggingObject != null) {
-
-//        draggingObject.GetComponent<Rigidbody>().MovePosition(CalculateMouse3DVector());
-//    }
-//}
-//else {
-
-//    if (draggingObject != null) {
-
-//        draggingObject.GetComponent<Rigidbody>().isKinematic = false;
-
-//    }
-
-//    isDragging = false;
-
-//}
-
-// OnMouseDrag() is called when the mouse clicks on the GameObject collider and continues to hold the mouse down
-//public void OnMouseDrag() {
-//    // Hold the item at a given distance from the camera while draging the user drags the mouse
-//    Vector3 mouseDrag = Input.mousePosition;
-//    mouseDrag.z = distance;
-//    mouseDrag = Camera.main.ScreenToWorldPoint(mouseDrag);
-
-//    this.gameObject.GetComponent<Rigidbody>().MovePosition(mouseDrag);
-//}
