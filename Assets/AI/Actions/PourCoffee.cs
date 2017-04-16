128bf0840bdf00a458a15b5acedc88d6554d2180
Chris.V <cvasqu09@gmail.com>
2017-04-16 10:58:42 -0500

    AI Changes animation state when reaching the coffee cup.

--- Assets/AI/Actions/PourCoffee.cs
+++ Assets/AI/Actions/PourCoffee.cs
@@ -9,22 +9,18 @@ public class PourCoffee : RAINAction
 {
 	GameObject barista;
 	AIScript script;
+	Animator anim;
 	public override void Start(RAIN.Core.AI ai)
     {
         base.Start(ai);
 		barista = GameObject.FindGameObjectWithTag ("AITag");
 		script = barista.GetComponent<AIScript> ();
+		anim = barista.GetComponent<Animator> ();
 	}
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
-		Animator anim = barista.GetComponent<Animator> ();
-		RuntimeAnimatorController newAnim = (RuntimeAnimatorController)Resources.Load (
-			                                    "Assets/Standard Assets/Character/Adam/Adam_IdleController");
-		if (newAnim == null) {
-			Debug.Log ("Anim not found");
-		}
-		anim.runtimeAnimatorController = newAnim;
+		anim.SetInteger ("state", 1);
 		return ActionResult.SUCCESS;
     }
 
