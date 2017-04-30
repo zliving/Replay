9546779b6a7faa028219be33cb45fb667ae816cc
Ray <reynaldo.garcia@ttu.edu>
2017-04-30 14:22:26 -0500

    Added music to our main menu

---
+++ Assets/Scripts/MenuMuisc.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MenuMuisc : MonoBehaviour {
+
+	static bool AudioBegin = false;
+	public AudioSource audio;
+
+	void Awake() {
+		if (!AudioBegin) {
+			audio.Play();
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		} 
+	}
+
+	void Update () {
+		if(Application.loadedLevelName == "Base Scene")
+		{
+			audio.Stop();
+			AudioBegin = false;
+		}
+	}
+}
