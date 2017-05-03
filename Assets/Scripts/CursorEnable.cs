7858631fe36ae7a1328e5552a1b288258ffa190e
Ray <reynaldo.garcia@ttu.edu>
2017-05-02 19:45:19 -0500

    Added a script that unlocks the cursor
    Added a script with the same lines of code that unlocks the cursor and makes it visible and attatched it to the main camera.

---
+++ Assets/Scripts/CursorEnable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorEnable : MonoBehaviour {
+	void Start () {
+		// Unlock the cursor and make it visible.
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+}
