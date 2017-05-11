af48e2ef8e1273f6381fbda9847860860730d76d
Chris.V <cvasqu09@gmail.com>
2017-05-11 01:25:52 -0500

    Made some changes to walk speed and volume

--- Assets/AI/Actions/OfCourse.cs
+++ Assets/AI/Actions/OfCourse.cs
@@ -7,16 +7,13 @@ using RAIN.Core;
 [RAINAction]
 public class OfCourse : RAINAction
 {
-	Dialogue audio;
     public override void Start(RAIN.Core.AI ai)
     {
         base.Start(ai);
-		audio = new Dialogue (ai, "Replay_Audio_5-2");
     }
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
-		audio.play ();
 		ai.WorkingMemory.SetItem<bool> ("timeUp", false);
 		ai.WorkingMemory.SetItem<bool> ("customerOrdered", false);
         return ActionResult.SUCCESS;
--- Assets/AI/Actions/OrderCoffee.cs
+++ Assets/AI/Actions/OrderCoffee.cs
@@ -7,19 +7,16 @@ using RAIN.Core;
 [RAINAction]
 public class OrderCoffee : RAINAction
 {
-	Dialogue audio;
 	AIRig barista;
     public override void Start(RAIN.Core.AI ai)
     {
         base.Start(ai);
-		audio = new Dialogue(ai, "Replay_Audio_1-2");
 		barista = GameObject.FindGameObjectWithTag ("AITag").GetComponentInChildren<AIRig> ();
     }
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
 		// Add Audio for ordering a coffee.
-		audio.play();
 		barista.AI.WorkingMemory.SetItem<bool> ("customerOrdered", true);
         return ActionResult.SUCCESS;
     }
